28d22674bc690c959676f2db253388e897f4b3b4
Mizuho Kobayashi <sin_emerald@hotmail.com>
2011-10-16 14:32:41 -0700

    reset_session other pages or cancels on admin only

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,5 +3,7 @@ class ApplicationController < ActionController::Base
 
   def reset_expiry
     session[:expiry] = Time.now + 20.minutes unless session[:user].nil? or session[:expiry] < Time.now
+    return true unless session[:user].blank? or session[:expiry] < Time.now
+    reset_session
   end
 end
