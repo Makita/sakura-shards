f71e6f6dae867db813b083b4c68ccd0c1fcaf798
Mizuho Kobayashi <sin_emerald@hotmail.com>
2011-10-16 03:13:58 -0700

    Have to reset session or admin functions work

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -8,6 +8,7 @@ class AdminController < ApplicationController
   def is_logged_in
     session[:expiry] = Time.now + 20.minutes unless session[:user].nil? or session[:expiry] < Time.now
     return true unless session[:user].blank? or session[:expiry] < Time.now
+    reset_session
     @title = "Login"
     render 'login'
   end
